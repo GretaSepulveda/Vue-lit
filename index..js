54a452fb62d77471b1d038f6a91b9d684393c184
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2021-04-07 12:29:28 -0500

    updates prop Defs

---
+++ index..js
@@ -0,0 +1,15 @@
+import { render } from 'http://unpkg.com/lit=html?module'
+import {
+shallowReactive,
+effect
+} from 'https://unpkg.com/@vue/reactivity.esm-browser.js'
+
+let currentInstance 
+
+export function defineComponent(name, propDefs, factory){
+    if (typeof propDefs==='function') {
+        factory = propDefs
+        propDefs = []
+    }
+
+    
