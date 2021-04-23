2e11e21f917738c0709ea431125fb86faf4aecfc
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2021-04-23 12:17:31 -0500

    update get let instance

--- index..js
+++ index..js
@@ -4,7 +4,7 @@ shallowReactive,
 effect 
 } from 'https://unpkg.com/@vue/reactivity.esm-browser.js'
 
-let currentInstance
+let 
 
 export function defineComponent(name, propDefs, factory){
     if (typeof propDefs==='function') {
