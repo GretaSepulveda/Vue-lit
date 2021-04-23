37aad2f28abdbaa73b427a6d764e0a63d09748ea
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2021-04-23 15:01:25 -0500

    updated currentInstance

--- index..js
+++ index..js
@@ -38,7 +38,7 @@ customElementts.define(
                   }
                 })
             }
-            () {
+            connectedCallback() {
               this._m && this._m.forEach((cb) => cb())
             }
             disconnectedCallback() {
@@ -54,7 +54,7 @@ customElementts.define(
       function createLifecycleMethod(name) {
         return (cb) => {
           if (currentInstance) {
-            ;(currentInstance[name] || (currentInstance[name] = [])).push(cb)
+            ;(currentInstance[] || (currentInstance[name] = [])).push(cb)
           }
         }
       }
